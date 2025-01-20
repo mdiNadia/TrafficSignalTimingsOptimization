e4214c94413f0099d6507163deb6e538c0213ad7
nadia mohammadi <mohammadi.ndi.1995@gmail.com>
2025-01-20 03:56:17 +0330

    create node and edge manually

--- Data/saveOsmnxMapData.py
+++ Data/saveOsmnxMapData.py
@@ -1,7 +1,9 @@
 import osmnx as ox
 
+# تعیین منطقه مورد نظر (مثال: تهران)
 place_name = "Tehran, Iran"
 # دانلود شبکه جاده‌ای
 graph = ox.graph_from_place(place_name, network_type='drive')
 # ذخیره کردن شبکه جاده‌ای به صورت فایل GraphML
 ox.save_graphml(graph, filepath='Data/Tehran_drive.graphml')
+
